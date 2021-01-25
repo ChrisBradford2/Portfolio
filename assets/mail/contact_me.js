a443a0a29c7ed105548a7b283d7bad388abf7673
Chris <nico.bar2012@gmail.com>
2021-01-25 22:45:41 +0100

    release : version 1.0

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -21,7 +21,7 @@ $(function () {
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                url: "localhost:80/www.nicolasbarbarisi.fr/assets/mail/contact_me.php",
+                url: "contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
@@ -39,7 +39,7 @@ $(function () {
                         )
                         .append("</button>");
                     $("#success > .alert-success").append(
-                        "<strong>Votre message à été envoyé avec succès ! </strong>"
+                        "<strong>Le serveur SMTP est en cours d'initialisation et sera opérationnel d'ici 48 heures. Si votre message est important, je vous invite à me contacter à l'adresse mail suivante : nicolas.barbarisi@ynov.com. </strong>"
                     );
                     $("#success > .alert-success").append("</div>");
                     //clear all fields
@@ -57,7 +57,7 @@ $(function () {
                         $("<strong>").text(
                             "Désolé " +
                                 firstName +
-                                ", il y a eu un problème avec l'envoi du mail. Je vous invite à me contacter à l'adresse mail suivante : nicolas.barbarisi@ynov.com."
+                                ", le serveur SMTP est en cours d'initialisation et sera opérationnel d'ici 48 heures. Si votre message est important, je vous invite à me contacter à l'adresse mail suivante : nicolas.barbarisi@ynov.com."
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
