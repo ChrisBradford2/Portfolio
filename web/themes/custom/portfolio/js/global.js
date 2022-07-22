4f0a256d78df4ecbadae57954c98c4d389209162
Nicolas Barbarisi <nico.bar2012@gmail.com>
2022-07-22 15:49:32 +0200

    Add experience-pro theming.

--- web/themes/custom/portfolio/js/global.js
+++ web/themes/custom/portfolio/js/global.js
@@ -9,7 +9,10 @@
 
   Drupal.behaviors.bootstrap_barrio_subtheme = {
     attach: function (context, settings) {
-
+      let job_card = document.querySelectorAll('.paragraph--type--experience-pro');
+      job_card.forEach((item, index) => {
+        (index % 2 === 0) ?item.classList.add('odd'):item.classList.add('even')
+      });
     }
   };
 
